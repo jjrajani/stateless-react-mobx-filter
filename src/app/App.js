b41958dfce2f941f7aa15483f498241169f554c3
humdrum <jjrajani@gmail.com>
2017-07-30 14:17:48 -0400

    restructure

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
-
-import USERS from './user/utils/users';
-import Filter from './filter/filter';
-import FilterByAgeUserList from './user/components/FilterByAgeUserList';
-
 import { observer } from 'mobx-react';
 import { observable } from 'mobx';
+
+import './App.css';
+
+import USERS from './components/user/utils/users';
+import Filter from './components/filter/filter';
+import FilterByAgeUserList from './components/user/components/FilterByAgeUserList';
 
 let age = observable(0);
 let changeAge = (filter) => {
